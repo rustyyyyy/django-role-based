23b7c192e0c2bb1efe6cdb4ed1db8a8c48657bfa
Ajulmaharjan <ajul@codepros.com.np>
2023-01-24 20:37:52 +0545

    updated db

--- properties/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Property
-# Register your models here.
-
-
-@admin.register(Property)
-class PropertyAdmin(admin.ModelAdmin):
-    model = Property
-    list_display = ["id", "user"]
--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import Dashboard, Login
-
-urlpatterns = [
-    path("login/", Login.as_view(), name="admin_login"),
-    path("", Dashboard.as_view(), name="dashboard"),
-]
