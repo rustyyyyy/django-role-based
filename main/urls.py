bcf70dc7fed9141ff37d70d9c30113ddf20c3f28
Ajulmaharjan <ajul@codepros.com.np>
2023-01-24 20:36:14 +0545

    fix: import sort

--- main/urls.py
+++ main/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path("super-admin/", admin.site.urls),
 ]
 
-# MEDIA_URL 
+urlpatterns += [path("admin/", include("users.urls"))]
+
+# MEDIA_URL
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
